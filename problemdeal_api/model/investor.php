b0579908d89db500af948410cb93d49f2a05ec33
Orgdevelopers <79126515+Orgdevelopers@users.noreply.github.com>
2022-05-09 07:57:02 -0700

    investor

--- problemdeal_api/model/investor.php
+++ problemdeal_api/model/investor.php
@@ -24,17 +24,41 @@ class investor{
 
         if(isset($data['sp'])){
             $sp = $data['sp'];
-            $qry = mysqli_query($this->conn, "SELECT * FROM investor ORDER BY id DESC LIMIT $sp,10 ;");
+            $qry = mysqli_query($this->conn, "SELECT investor.*,
+            investor.category AS category_id,
+            users.username,
+            category.name AS category_name,
+            category.icon AS category_icon
+            FROM
+            investor, users, category
+            WHERE investor.user_id = users.id
+            AND investor.category = category.id ORDER BY id DESC LIMIT $sp,10 ;");
         }
 
         if(isset($data['all'])){
-            $qry = mysqli_query($this->conn, "SELECT * FROM investor ORDER BY id DESC");
+            $qry = mysqli_query($this->conn, "SELECT investor.*,
+            investor.category AS category_id,
+            users.username,
+            category.name AS category_name,
+            category.icon AS category_icon
+            FROM
+            investor, users, category
+            WHERE investor.user_id = users.id
+            AND investor.category = category.id ORDER BY id DESC");
 
         }
 
         if(isset($data['sp']) && isset($data['all'])){
             $sp = $data['sp'];
-            $qry = mysqli_query($this->conn, "SELECT * FROM investor ORDER BY id DESC LIMIT $sp,1000");
+            $qry = mysqli_query($this->conn, "SELECT investor.*,
+            investor.category AS category_id,
+            users.username,
+            category.name AS category_name,
+            category.icon AS category_icon
+            FROM
+            investor, users, category
+            WHERE investor.user_id = users.id
+            AND investor.category = category.id ORDER BY id DESC LIMIT $sp,1000");
 
         }
         return mysqli_fetch_all($qry,1);
